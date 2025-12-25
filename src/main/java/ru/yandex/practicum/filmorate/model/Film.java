b2c73ffdd67aafefc01e4b4b7513ae419eec8442
Daniil.Churilov <Heling53@yandex.ru>
2025-12-25 13:57:09 +0300

    Добавлен сет для лайков

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -7,6 +7,8 @@ import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 public class Film {
@@ -20,4 +22,5 @@ public class Film {
     @NotNull(message = "Продолжительность фильма должна быть указана")
     @Positive(message = "Продолжительность фильма должна быть положительным числом")
     private Integer duration;
+    private Set<Integer> likes = new HashSet<>();
 }
