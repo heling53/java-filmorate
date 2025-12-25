03a34177451750cc2e05bdf448170c71a8262255
Daniil.Churilov <Heling53@yandex.ru>
2025-12-25 14:03:35 +0300

    Интерфейс хранилища

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.filmorate.storage.film;
+
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.List;
+
+public interface FilmStorage {
+    List<Film> getAllFilms();
+    Film createFilm(Film film);
+    Film updateFilm(Film film);
+    Film getFilmById(Integer id);
+}
