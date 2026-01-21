5a7ecac9dc4deede73626a0d8dd3617e8626bd78
Daniil.Churilov <Heling53@yandex.ru>
2026-01-21 09:30:41 +0300

    Новые методы в соответсвии с ТЗ

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,94 +1,45 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
-@Slf4j
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class FilmService {
+
     private final FilmStorage filmStorage;
-    private final UserService userService;
-
-    @Autowired
-    public FilmService(FilmStorage filmStorage, UserService userService) {
-        this.filmStorage = filmStorage;
-        this.userService = userService;
-    }
-
-    public void addLike(Integer filmId, Integer userId) {
-        log.info("Добавление лайка фильму {} от пользователя {}", filmId, userId);
-        Film film = getFilmById(filmId);
-        userService.getUserById(userId);
-
-        if (film.getLikes().contains(userId)) {
-            log.warn("Пользователь {} уже ставил лайк фильму {}", userId, filmId);
-            throw new IllegalArgumentException("Пользователь уже ставил лайк этому фильму");
-        }
-        film.getLikes().add(userId);
-        log.info("Лайк добавлен: фильм {}, пользователь {}", filmId, userId);
-    }
-
-    public void removeLike(Integer filmId, Integer userId) {
-        log.info("Удаление лайка фильму {} от пользователя {}", filmId, userId);
-        Film film = getFilmById(filmId);
-        userService.getUserById(userId);
-
-        if (!film.getLikes().contains(userId)) {
-            log.warn("Пользователь {} не ставил лайк фильму {}", userId, filmId);
-            throw new IllegalArgumentException("Пользователь не ставил лайк этому фильму");
-        }
-        film.getLikes().remove(userId);
-        log.info("Лайк удален: фильм {}, пользователь {}", filmId, userId);
-    }
-
-    public List<Film> getPopularFilms(Integer count) {
-        int limit = (count != null) ? count : 10;
-        log.info("Получение {} самых популярных фильмов", limit);
-        return filmStorage.getAllFilms().stream()
-                .sorted(Comparator.comparingInt(
-                        (Film film) -> film.getLikes() != null ? film.getLikes().size() : 0
-                ).reversed())
-                .limit(limit)
-                .collect(Collectors.toList());
-
-    }
-
-    public Film getFilmById(Integer id) {
-        log.debug("Поиск фильма с id={}", id);
-        Film film = filmStorage.getFilmById(id);
-        if (film == null) {
-            log.error("Фильм с id={} не найден", id);
-            throw new NotFoundException("Фильм с указанным id не найден");
-        }
-        return film;
-    }
-
-    public List<Film> getAllFilms() {
-        log.info("Получение всех фильмов");
-        return filmStorage.getAllFilms();
-    }
 
     public Film createFilm(Film film) {
-        log.info("Создание фильма: {}", film);
         return filmStorage.createFilm(film);
     }
 
     public Film updateFilm(Film film) {
-        log.info("Обновление фильма: {}", film);
-
-        if (filmStorage.getFilmById(film.getId()) == null) {
-            log.error("Фильм с id={} не найден для обновления", film.getId());
-            throw new NotFoundException("Фильм с указанным id не найден");
-        }
         return filmStorage.updateFilm(film);
     }
 
+    public List<Film> getAllFilms() {
+        return filmStorage.getAllFilms();
+    }
+
+    public Film getFilmById(Integer id) {
+        return filmStorage.getFilmById(id);
+    }
+
+    public void addLike(Integer filmId, Integer userId) {
+        filmStorage.addLike(filmId, userId);
+    }
+
+    public void removeLike(Integer filmId, Integer userId) {
+        filmStorage.removeLike(filmId, userId);
+    }
+
+    public List<Film> getPopularFilms(Integer count) {
+        return filmStorage.getPopularFilms(count);
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -1,110 +1,53 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
-@Slf4j
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class UserService {
+
     private final UserStorage userStorage;
 
-    public UserService(UserStorage userStorage) {
-        this.userStorage = userStorage;
-    }
-
-    public void addFriend(Integer userId, Integer friendId) {
-        log.info("Добавление пользователя {} в друзья пользователю {}", friendId, userId);
-
-        User user = getUserById(userId);
-        User friend = getUserById(friendId);
-
-        if (user.getFriends().contains(friendId)) {
-            log.warn("Пользователь {} уже в друзьях у {}", friendId, userId);
-            throw new IllegalArgumentException("Пользователь уже в друзьях");
-        }
-        user.getFriends().add(friendId);
-        friend.getFriends().add(userId);
-
-        log.info("Пользователи {} и {} теперь друзья", userId, friendId);
-
-    }
-
-    public User getUserById(Integer id) {
-        log.debug("Поиск пользователя с id={}", id);
-        User user = userStorage.getUserById(id);
-        if (user == null) {
-            log.error("Пользователь с id={} не найден", id);
-            throw new NotFoundException("Пользователь с указанным id не найден");
-        }
-        return user;
-    }
-
-    public List<User> getFriends(Integer userId) {
-        log.info("Получение списка друзей пользователя {}", userId);
-        User user = getUserById(userId);
-        return user.getFriends().stream()
-                .map(this::getUserById)
-                .collect(Collectors.toList());
-    }
-
-    public void removeFriend(Integer userId, Integer friendId) {
-        log.info("Удаление пользователя {} из друзей пользователя {}", friendId, userId);
-
-        User user = getUserById(userId);
-        User friend = getUserById(friendId);
-
-        if (!user.getFriends().contains(friendId)) {
-            log.warn("Пользователь {} не в друзьях у {}", friendId, userId);
-            return;
-
-        }
-        user.getFriends().remove(friendId);
-        friend.getFriends().remove(userId);
-
-        log.info("Пользователи {} и {} больше не друзья", userId, friendId);
-    }
-
-    public List<User> getAllUsers() {
-        log.info("Получение всех пользователей");
-        return userStorage.getAllUsers();
-    }
-
     public User createUser(User user) {
-        log.info("Создание пользователя: {}", user);
         if (user.getName() == null || user.getName().isBlank()) {
             user.setName(user.getLogin());
-            log.debug("Имя пользователя не задано, установлено значение логина: {}", user.getLogin());
         }
         return userStorage.createUser(user);
     }
 
     public User updateUser(User user) {
-        getUserById(user.getId());
-        if (user.getName() == null || user.getName().isBlank()) {
-            user.setName(user.getLogin());
-        }
         return userStorage.updateUser(user);
     }
 
-    public List<User> getCommonFriends(Integer userId, Integer otherId) {
-        log.info("Получение общих друзей пользователей {} и {}", userId, otherId);
+    public List<User> getAllUsers() {
+        return userStorage.getAllUsers();
+    }
 
-        User user = getUserById(userId);
-        User other = getUserById(otherId);
+    public User getUserById(Integer id) {
+        return userStorage.getUserById(id);
+    }
 
-        Set<Integer> commonIds = new HashSet<>(user.getFriends());
-        commonIds.retainAll(other.getFriends());
+    public void addFriend(Integer userId, Integer friendId) {
+        userStorage.addFriend(userId, friendId);
+    }
 
-        return commonIds.stream()
-                .map(this::getUserById)
-                .collect(Collectors.toList());
+    public void removeFriend(Integer userId, Integer friendId) {
+        userStorage.removeFriend(userId, friendId);
+    }
+
+    public List<User> getFriends(Integer userId) {
+        return userStorage.getFriends(userId);
+    }
+
+    public List<User> getCommonFriends(Integer userId1, Integer userId2) {
+        return userStorage.getCommonFriends(userId1, userId2);
     }
 }
